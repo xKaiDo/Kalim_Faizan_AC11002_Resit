76419513998e90a183a32edf74f599abe1caf4ed
xKaiDo <46615060+xKaiDo@users.noreply.github.com>
2020-07-12 01:53:23 +0100

    Implemented the index page and the files that are going to contain the CSS and the JavaScript

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Kalim_Faizan_AC11002_Resit
  Web application with pre-existing webAPI
+
+ Created by Faizan Kalim
---
+++ app.js
@@ -0,0 +1,4 @@
+const api = {
+  key: "2c2bb4073956d6a41957a6126b85e782",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
